fe9dcd97cef12ac853d5ba69dd8d10fdc623945d
drewbaetazar <baltazarcode@gmail.com>
2021-04-17 17:30:46 -0700

    modified platform prefab, added platform spawner empty object + scrip

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(platformWave());
+    }
+
+    private void spawnPlatform() {
+        GameObject platform = Instantiate(platformPrefab) as GameObject;
+        platform.transform.position = new Vector2(screenBounds.x, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator platformWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            spawnPlatform();
+        }
+    
+    }
+}
